e9a9c95a084ab8ccfdb82a811c07673a793de0e2
Liam Daley <liam.daley@synergy.net.au>
2020-08-24 10:42:37 +0800

    SQL Server and the Entity Framework Core

---
+++ OdeToFood.Data/InMemoryRestaurantData.cs
@@ -0,0 +1,68 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+using static OdeToFood.Core.Restaurant;
+
+namespace OdeToFood.Data
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Peter's Pizza", Location = "Perth", Cuisine = CuisineType.Italian},
+                new Restaurant { Id = 2, Name = "Bobs's Burgers", Location = "Melbourne", Cuisine = CuisineType.Mexican},
+                new Restaurant { Id = 3, Name = "Patrick's Pies", Location = "Sydney", Cuisine = CuisineType.Indian}
+            };
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if (restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+            return restaurant;
+        }
+
+        public int Commit()
+        {
+            return 0;
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var restaurant = restaurants.FirstOrDefault(r => r.Id == id);
+            if (restaurant != null)
+            {
+                restaurants.Remove(restaurant);
+            }
+            return restaurant;
+        }
+    }
+}
